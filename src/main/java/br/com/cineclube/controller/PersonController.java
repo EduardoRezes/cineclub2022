545ff61969d6e1fb73771072bd883b84d0592cec
Eduardo <contatoeduardorezes@gmail.com>
2022-02-24 07:15:25 -0300

    Feature - Criação de page List e Manter

--- src/main/java/br/com/cineclube/controller/PersonController.java
+++ src/main/java/br/com/cineclube/controller/PersonController.java
@@ -1,11 +1,15 @@
 package br.com.cineclube.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,19 +23,44 @@ public class PersonController {
 	@Autowired
 	private PersonRepository daoPerson;
 	
+	/* LIST PERSON */
+	@GetMapping("/list")
+	public String list(Model model) {
+		List<Person> listPerson = daoPerson.findAll();
+		model.addAttribute("personList", listPerson);
+		return "person/listPerson.html";
+	}
 
 	/* NEW PERSON */
 	@PostMapping("/new")
 	public String newPerson(Model model) {
 		Person p = new Person();
-		model.addAttribute(p);
-		return "person/manterPerson";
+		model.addAttribute("person", p);
+		return "person/manterPerson.html";
 	}
 	
 	/* SAVE PERSON */
+	@PostMapping("/save")
+	public String save(@Valid Person person, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return"person/manterPerson";
+		}
+		daoPerson.save(person);
+		return "redirect:/persons/list";
+	}
 	
 	/* EDIT PERSON */
-
-	/* DELETE PERSON */
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Long id, Model model) {
+		Person person = daoPerson.findById(id).get();
+		model.addAttribute("person", person);
+		return "person/manterPerson.html";
+	}
 	
+	/* DELETE PERSON */
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Long id) {
+		daoPerson.deleteById(id);
+		return "redirect:/persons/list";
+	}
 }
